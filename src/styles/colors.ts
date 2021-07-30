79f863370b771525177d4c2875b4ddb579abcf69
Weslley Oliveira <weslleyzera2020@gmail.com>
2021-07-30 22:40:44 +0100

    :art: Create Styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalColors = createGlobalStyle`
+    :root {
+        --background: #030E1D;
+        --board-game: #7C5E99;
+        --card-back: #051932;
+        --card-front: #F25A70;
+        --player-score: #62C370;
+        --font-color: #FFFCEE;
+    }
+`
---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+	html {
+		background-color: var(--background);
+		font-family: 'Press Start 2P', cursive;
+		color: white;
+	}
+
+	body {
+		display: flex;
+		justify-content: center;
+	}
+	
+`
